1f8f43f47bd21e0ffb003086df25316443f35565
Chough <carter.hough@hotmail.com>
2018-12-10 20:58:29 -0700

    Re-added mongoose data validation

--- server/models/buyer.js
+++ server/models/buyer.js
@@ -1,16 +1,35 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator')
 const Schema = mongoose.Schema;
 
 var BuyerSchema = new Schema({
-  bidderNumber: Number,
-  name: String,
-  contactName: String,
-  phone: String,
-  email: String,
-  logoFileName: String
+  bidderNumber: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  contactName: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+  },
+  logoFileName: {
+    type: String
+  }
 },{
     collection: 'Buyer'
 });
 
+BuyerSchema.plugin(uniqueValidator);
 var Buyer = mongoose.model('Buyer', BuyerSchema);
 module.exports = Buyer;
